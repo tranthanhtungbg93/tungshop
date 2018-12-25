b67ae51c17e49dca32c52e34f4ac9d66e5ae5be1
tung tran <tranthanhtung.cd2.21@gmail.com>
2018-12-25 17:25:35 +0700

    add router product

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// 
+
+(function () {
+    angular.module('tedushop', ['tedushop.product','tedushop.common']).config(config);
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/home/index",
+            templateUrl: "/app/component/home/home.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/home/index');
+    }
+})();
---
+++ TeduShop.Web/app/component/product/product.module.js
@@ -0,0 +1,22 @@
+﻿/// 
+
+(function () {
+    angular.module('tedushop.product', ['tedushop.common']).config(config);
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('product_list', {
+            url: "/product_list",
+            templateUrl: "/app/component/product/productListView.html",
+            controller: "productListController"
+        }).state('product_edit', {
+            url: "/product_edit",
+            templateUrl: "/app/component/product/productEditView.html",
+            controller: "productEditController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/component/product/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
